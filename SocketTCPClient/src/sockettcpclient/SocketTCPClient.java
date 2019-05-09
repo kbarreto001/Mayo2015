3b77eb96034f1fd0ad9de30c1a8fa57840191c7f
kbarreto001 <kbarreto001@ikasle.ehu.eus>
2019-05-09 15:41:42 +0200

    tcp done

--- SocketTCPClient/src/sockettcpclient/SocketTCPClient.java
+++ SocketTCPClient/src/sockettcpclient/SocketTCPClient.java
@@ -1,13 +1,36 @@
 package sockettcpclient;
 
-public class SocketTCPClient {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
 
-    public static void main(String[] args) {
-        String[] nombreyP = new String[2];
-        nombreyP[0] = "ubuntu";
-        nombreyP[1] = "5000";
-        System.out.println("Soy el cliente"); 
-        connectionC otro = new connectionC();
-        otro.connectionC(nombreyP);
-    }
+public class SocketTCPClient {
+    
+    protected static File FICHEROT = new File("File02T.txt");    
+
+    public static void main(String[] args) throws IOException {
+        String[] ficheroVolcado;
+        String infoFinal,nombreFinal;
+        try(
+                Socket client = new Socket("localhost",5000);
+                DataInputStream disSocket = new DataInputStream(client.getInputStream());
+                DataOutputStream dosSocket = new DataOutputStream(client.getOutputStream());
+                ){
+            System.out.println("Soy el cliente");
+            SupportC otro = new SupportC();
+            
+            otro.EnviarNombre(FICHEROT, dosSocket);
+            ficheroVolcado=otro.VolcarFichero(FICHEROT);            
+            otro.EnviarTamano(ficheroVolcado[0],dosSocket);            
+            otro.EnviarFicheroT(ficheroVolcado, dosSocket);
+            
+            nombreFinal = disSocket.readUTF();            
+            infoFinal = disSocket.readUTF();
+            System.out.println(infoFinal);
+            otro.GuardarFicheroB(otro.ficheroFinal(nombreFinal), infoFinal);
+            
+        }
+    }   
 }
---
+++ SocketTCPClient/src/sockettcpclient/SupportC.java
@@ -0,0 +1,73 @@
+package sockettcpclient;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class SupportC {
+
+    public String[] VolcarFichero(File FICHEROT) throws FileNotFoundException, IOException {
+        String[] ficheroVolcado;
+        int tmp1 = 0;
+        String tmp2;
+        try (
+                BufferedReader dis = new BufferedReader(new FileReader(FICHEROT));
+                BufferedReader disT = new BufferedReader(new FileReader(FICHEROT));) {
+            while ((tmp2 = disT.readLine()) != null) {
+                tmp1++;
+            }
+            ficheroVolcado = new String[tmp1];
+            for (int i = 0; i < ficheroVolcado.length; i++) {
+                ficheroVolcado[i] = dis.readLine();
+            }
+        }
+        return ficheroVolcado;
+    }
+
+    public void EnviarNombre(File FICHEROT, DataOutputStream dosSocket) throws IOException {
+        try {
+            dosSocket.writeUTF(FICHEROT.getName());
+        } catch (IOException ex) {
+            System.out.println("Error 2: " + ex.getLocalizedMessage());
+        }
+    }
+
+    public void EnviarFicheroT(String[] ficheroVolcado, DataOutputStream dosSocket) throws IOException {
+        try {
+            for (int i = 0; i < ficheroVolcado.length; i++) {
+                dosSocket.writeUTF(ficheroVolcado[i]);
+            }
+        } catch (IOException ex) {
+            System.out.println("Error 2: " + ex.getLocalizedMessage());
+        }
+    }
+
+    public void EnviarTamano(String numFilas, DataOutputStream dosSocket) throws IOException {
+        String[] tmp = new String[2];
+        tmp = numFilas.split(" ");
+        try {
+            dosSocket.writeUTF(tmp[0]);
+        } catch (IOException ex) {
+            System.out.println("Error 2: " + ex.getLocalizedMessage());
+        }
+    }
+
+    public void GuardarFicheroB(File FICHEROB, String infoFinal) throws FileNotFoundException, IOException {
+        try (
+            DataOutputStream dos = new DataOutputStream(new FileOutputStream(FICHEROB));) {
+            dos.writeUTF(infoFinal);
+        }
+    }
+    
+    public File ficheroFinal(String nombreFinal){
+        File FICHEROB;
+        nombreFinal = nombreFinal.replace("T", "B");        
+        return FICHEROB = new File(nombreFinal);
+    }
+
+}
